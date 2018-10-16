9a031133c96c16146b3a3778a8636748bc608adc
David <beaux@colourcountry.net>
2018-10-16 17:00:48 +0100

    separate false from the specific website

--- false.py
+++ false.py
@@ -8,6 +8,8 @@ import jinja2, markdown
 import pprint
 from triplate import *
 
+logging.basicConfig(level=logging.DEBUG)
+
 FALSE_SRC = os.environ["FALSE_SRC"]
 FALSE_URL_BASE = os.environ["FALSE_URL_BASE"]
 FALSE_OUT = os.environ["FALSE_OUT"]
@@ -15,23 +17,23 @@ FALSE_TEMPLATES = os.environ["FALSE_TEMPLATES"]
 
 IPFS_CLIENT = ipfsapi.connect('127.0.0.1',5001)
 FILE_TYPES = { ".html": "text/html", ".txt": "text/plain", ".md": "text/markdown" }
-CC = rdflib.Namespace("http://www.colourcountry.net/thing/")
+F = rdflib.Namespace("http://www.colourcountry.net/false/model/")
 IPFS = rdflib.Namespace("/ipfs/")
 
 
 def addRendition(g, doc_id, blob, **properties):
     r = IPFS_CLIENT.add_bytes(blob)
     blob_id = IPFS[r]
-    g.add((blob_id, RDF.type, CC['Media']))
+    g.add((blob_id, RDF.type, F['Media']))
     for k, v in properties.items():
-        g.add((blob_id, CC[k], v))
-    g.add((doc_id, CC['rendition'], blob_id))
+        g.add((blob_id, F[k], v))
+    g.add((doc_id, F['rendition'], blob_id))
     return blob_id
 
 
 def buildGraph(g):
     gg = rdflib.Graph()
-    gg.bind('cc', CC)
+    gg.bind('f', F)
     gg.bind('dc', DC)
     gg.bind('skos', SKOS)
     gg.bind('owl', OWL)
@@ -44,15 +46,15 @@ def buildGraph(g):
         if s not in ENTS:
             ENTS[s] = s
         if p == RDF.type:
-            if o == CC['Document']:
-                ENTS[s] = CC[os.path.basename(s)]
+            if o == F['Document']:
+                ENTS[s] = F[os.path.basename(s)]
                 DOCS[s] = ENTS[s]
 
     for s, p, o in g:
         if o in ENTS:
             o = ENTS[o]
 
-        if p == CC['markdown']:
+        if p == F['markdown']:
             blob_id = addRendition(gg, DOCS[s], o.encode('utf-8'),
                 mediaType=rdflib.Literal('text/markdown'),
                 charset=rdflib.Literal('utf-8')
@@ -82,6 +84,7 @@ def publish(g):
     tg = TemplatableGraph(g)
 
     for e in tg.entities:
+        logging.debug('Publishing %s' % e)
         for eType in tg.entities[e].rdf_type:
             try:
                 with open(os.path.join(FALSE_TEMPLATES,eType.safe),'r') as f:
@@ -91,14 +94,14 @@ def publish(g):
                 continue
 
             dest = getDestPath(e, eType)
-            logging.warn('Building %s' % dest)
+            logging.info('Building %s %s' % (eType.safe, dest))
 
-            for r in tg.entities[e].cc_rendition:
-                mt = r.cc_mediaType
-                enc = r.cc_charset.pop()
+            for r in tg.entities[e].f_rendition:
+                mt = r.f_mediaType
+                enc = r.f_charset.pop()
 
                 if rdflib.Literal('text/markdown') in mt:
-                    tg.add(tg.entities[e].id, CC.html, rdflib.Literal(md.convert(IPFS_CLIENT.cat(r.id).decode(enc))))
+                    tg.add(tg.entities[e].id, F.html, rdflib.Literal(md.convert(IPFS_CLIENT.cat(r.id).decode(enc))))
                     content = t.render(tg.entities[e].po)
                 elif rdflib.Literal('text/html') in mt:
                     content = IPFS_CLIENT.cat(r.id).decode(enc)
@@ -107,6 +110,7 @@ def publish(g):
 
             try:
                 os.makedirs(os.path.dirname(dest), exist_ok=True)
+                logging.warn("Writing %s" % dest)
                 with open(dest,'w') as f:
                     f.write(content)
             except IOError as e:
