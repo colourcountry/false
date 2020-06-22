fdc3f0aa81f33fe7eed862d2a8c82be2371474e0
dave <git@colourcountry.net>
2020-06-22 07:28:05 +0100

    drastic speedup by using diff properly and sharing work dir between build and publish

--- convert_images.sh
+++ convert_images.sh
@@ -1,69 +1,61 @@
 #!/bin/bash
 
-export TEASER_WIDTH=400
-export TEASER_HEIGHT=225
-export EMBED_SIZE=800
-export PAGE_SIZE=1200
+echo "Looking in '$SRC_DIR' for blobs to render to '$BLOB_DIR'"
+echo "- rendering & cropping teasers to $TEASER_WIDTH x $TEASER_HEIGHT px"
+echo "- rendering embeds to $EMBED_SIZE px"
+echo "- rendering pages to $PAGE_SIZE px"
 
-if [[ $2 == "-wipe" ]]
-then
-    echo "Removing all cached conversions"
-    find "$1" -name '.teaser.*'\
-              -or -name '.embed.*'\
-              -or -name '.page.*'\
-              -or -name '.download.*' | while read FILE
-    do
-        rm "$FILE"
-    done
-fi
+mkdir -p "$BLOB_DIR"
+pushd "$BLOB_DIR"
+export FULL_BLOB_DIR=$(pwd)
+popd > /dev/null
 
-find "$1" -name '.download.ipfs-*' | while read FILE
-do
-    pushd $(dirname $FILE) > /dev/null
-    export ORIG_FILE=$(basename "$FILE" | sed -e 's/[.]download[.]ipfs-[^.]*[.]//')
-    if [[ -f "$ORIG_FILE" ]]
-    then
-        if [[ $2 != "-nochecks" ]]
-        then
-            echo "$ORIG_FILE: checking for changes"
-            diff -q $(basename "$FILE") "$ORIG_FILE" || rm -fv .*."$ORIG_FILE"
-        fi
-    else
-        echo "$ORIG_FILE has been removed."
-        rm -fv .*."$ORIG_FILE"
-    fi
-    popd > /dev/null
-done
-
-find "$1" -not -name '.*'\
+pushd "$SRC_DIR"
+find .    -not -name '.ttl'\
           -not -name 'teaser.*.*'\
           -not -name 'embed.*.*'\
           -not -name 'page.*.*'\
           -regextype posix-awk -regex '.*.(jpg|png)' | while read IMAGE
 do
-    pushd $(dirname $IMAGE) > /dev/null
+    export D=$(dirname $IMAGE)
     export B=$(basename $IMAGE)
+    echo "$D -- $B"
 
-    if find -type f -name '.download.*.'"$B" -exec false {} +
+    if find "$FULL_BLOB_DIR" -type f -name 'download.*.'"$B" -exec false {} +
     then
-        cp "$B" .download.ipfs-$(ipfs add -Q "$B")."$B"
+        mkdir -p "$FULL_BLOB_DIR"/"$D"
+        cp "$IMAGE" "$FULL_BLOB_DIR"/"$D"/download.unknown."$B"
     fi
-    if find -type f -name '.teaser.*.'"$B" -exec false {} +
-    then
-        convert -verbose -auto-orient -resize "$TEASER_WIDTH"x"$TEASER_HEIGHT"^ -gravity center -crop "$TEASER_WIDTH"x"$TEASER_HEIGHT"+0+0 "$B" .teaser.tmp
-        mv .teaser.tmp .teaser.ipfs-$(ipfs add -Q .teaser.tmp)."$B"
-    fi
+    #if find "$BLOB_DIR" -type f -name 'teaser.*.'"$B" -exec false {} +
+    #then
+    #    convert -verbose -auto-orient -resize "$TEASER_WIDTH"x"$TEASER_HEIGHT"^ -gravity center -crop "$TEASER_WIDTH"x"$TEASER_HEIGHT"+0+0 "$B" .teaser.tmp
+    #    mv .teaser.tmp "$ORIG_DIR/$BLOB_DIR/.teaser.ipfs-$(ipfs add -Q .teaser.tmp)."$B"
+    #fi
 
-    if find -type f -name '.embed.*.'"$B" -exec false {} +
-    then
-        convert -verbose -auto-orient -resize "$EMBED_SIZE"x"$EMBED_SIZE"\> "$B" .embed.tmp
-        mv .embed.tmp .embed.ipfs-$(ipfs add -Q .embed.tmp)."$B"
-    fi
+    #if find -type f -name '.embed.*.'"$B" -exec false {} +
+    #then
+    #    convert -verbose -auto-orient -resize "$EMBED_SIZE"x"$EMBED_SIZE"\> "$B" .embed.tmp
+    #    mv .embed.tmp .embed.ipfs-$(ipfs add -Q .embed.tmp)."$B"
+    #fi
 
-    if find -type f -name '.page.*.'"$B" -exec false {} +
-    then
-        convert -verbose -auto-orient -resize "$PAGE_SIZE"x"$PAGE_SIZE"\> "$B" .page.tmp
-        mv .page.tmp .page.ipfs-$(ipfs add -Q .page.tmp)."$B"
-    fi
-    popd > /dev/null
+    #if find -type f -name '.page.*.'"$B" -exec false {} +
+    #then
+    #    convert -verbose -auto-orient -resize "$PAGE_SIZE"x"$PAGE_SIZE"\> "$B" .page.tmp
+    #    mv .page.tmp .page.ipfs-$(ipfs add -Q .page.tmp)."$B"
+    #fi
 done
+popd > /dev/null
+
+pushd "$FULL_BLOB_DIR"
+find . -name "*.unknown.*" | while read FILE
+do
+  export IPFS_HASH=$(ipfs add -Q "$FILE")
+
+  if [ -n "$IPFS_HASH" ]
+  then
+    export D=$(dirname "$FILE")
+    export B=$(basename "$FILE")
+    mv "$FILE" "$D"/$(echo "$B" | sed -e s/[.]unknown[.]/.ipfs-"$IPFS_HASH"./)
+  fi
+done
+popd > /dev/null
